757c2e1f04a92944604d3e470a9552fc33051444
West6879 <west13731@gmail.com>
2025-08-13 22:54:50 -0400

    Recipes, loot tables, advanced items and blocks

--- src/main/java/net/west6879/firstmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/west6879/firstmod/item/ModCreativeModeTabs.java
@@ -21,9 +21,21 @@ public class ModCreativeModeTabs {
                     .displayItems((itemDisplayParameters, output) -> {
                         output.accept(ModItems.SAPPHIRE.get());
                         output.accept(ModItems.RAW_SAPPHIRE.get());
+
+                        output.accept(ModItems.METAL_DETECTOR.get());
+
                         output.accept(Items.DIAMOND);
+
                         output.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         output.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                        output.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+
+                        output.accept(ModBlocks.SOUND_BLOCK.get());
+
                     })
                     .build());
 
