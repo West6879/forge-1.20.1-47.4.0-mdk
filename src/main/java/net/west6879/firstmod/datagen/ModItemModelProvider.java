00841422cd083d6e61076d4064cc329b60121ca3
West6879 <west13731@gmail.com>
2025-08-15 09:30:28 -0400

    Datagen

---
+++ src/main/java/net/west6879/firstmod/datagen/ModItemModelProvider.java
@@ -0,0 +1,32 @@
+package net.west6879.firstmod.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.west6879.firstmod.FirstMod;
+import net.west6879.firstmod.item.ModItems;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, FirstMod.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ModItems.SAPPHIRE);
+        simpleItem(ModItems.RAW_SAPPHIRE);
+        simpleItem(ModItems.METAL_DETECTOR);
+        simpleItem(ModItems.STRAWBERRY);
+        simpleItem(ModItems.PINE_CONE);
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                ResourceLocation.tryParse("item/generated")).texture("layer0",
+                ResourceLocation.tryBuild(FirstMod.MODID, "item/" + item.getId().getPath()));
+    }
+}
---
+++ src/main/java/net/west6879/firstmod/datagen/ModLootTableProvider.java
@@ -0,0 +1,17 @@
+package net.west6879.firstmod.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.west6879.firstmod.datagen.loot.ModBlockLootTables;
+
+import java.util.List;
+import java.util.Set;
+
+public class ModLootTableProvider {
+    public static LootTableProvider create(PackOutput output) {
+        return new LootTableProvider(output, Set.of(), List.of(
+                new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)
+        ));
+    }
+}
